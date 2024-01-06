ae6be914ecc9175988d5543c8c98357dba38510a
Vitalii <havryliv.v@icloud.com>
2024-01-06 20:33:20 +0200

    refactoring to multiple plugins

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom"
 import { useEffect, useRef, useState } from "react"
 import * as esbuild from "esbuild-wasm"
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin"
+import { fetchPlugin } from "./plugins/fetch-plugin"
 
 const App = () => {
   const [input, setInput] = useState("")
@@ -27,7 +28,7 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
       define: {
         "process.env.NODE_ENV": '"production"',
         global: "window",
--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,17 +1,6 @@
 import * as esbuild from "esbuild-wasm"
-import axios from "axios"
-import localForage from "localforage"
 
-const fileCache = localForage.createInstance({
-  name: "filecache",
-})
-;(async () => {
-  await fileCache.setItem("color", "red")
-
-  const color = await fileCache.getItem("color")
-  console.log(color)
-})()
-export const unpkgPathPlugin = (inputCode: string) => {
+export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
@@ -34,33 +23,6 @@ export const unpkgPathPlugin = (inputCode: string) => {
         }
       })
       //Handle main file of the module
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log("onLoad", args)
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: inputCode,
-          }
-        }
-
-        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
-          args.path
-        )
-
-        if (cachedResult) {
-          return cachedResult
-        }
-
-        const { data, request }: any = await axios.get(args.path)
-        const result: esbuild.OnLoadResult = {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname,
-        }
-        await fileCache.setItem(args.path, result)
-        return result
-      })
     },
   }
 }
