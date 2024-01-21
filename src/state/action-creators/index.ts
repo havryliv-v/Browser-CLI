b213d07040fed47deba8e3de13fba4e0772fc0d3
Vitalii <havryliv.v@icloud.com>
2024-01-21 11:43:33 +0200

    finishing implementing Redux store to the project

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,50 @@
+import { ActionType } from "../action-types"
+import {
+  Action,
+  Direction,
+  MoveCellAction,
+  DeleteCellAction,
+  InsertCellBeforeAction,
+  UpdateCellAction,
+} from "../actions"
+import { CellTypes } from "../cell"
+
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  }
+}
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  }
+}
+
+export const InsertCellBefore = (
+  id: string,
+  type: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type,
+    },
+  }
+}
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  }
+}
