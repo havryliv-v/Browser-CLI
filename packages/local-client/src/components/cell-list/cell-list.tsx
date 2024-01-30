a4c2e36cad2f7686e3052d56b6a9692096561827
Vitalii <havryliv.v@icloud.com>
2024-01-30 21:48:02 +0200

    adding lerna to project

--- packages/local-client/src/components/cell-list/cell-list.tsx
+++ packages/local-client/src/components/cell-list/cell-list.tsx
@@ -1,3 +1,5 @@
+import "./cell-list.css"
+
 import { useTypedSelector } from "../../hooks/use-typed-selector"
 import CellListItem from "../cell-list-item/cell-list-item"
 import AddCell from "../add-cell/add-cell"
@@ -15,7 +17,7 @@ const CellList: React.FC = () => {
   ))
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
--- packages/local-client/src/components/code-cell/code-cell.tsx
+++ packages/local-client/src/components/code-cell/code-cell.tsx
@@ -8,6 +8,7 @@ import Resizable from "../resizable/resizable"
 import { Cell } from "../../state"
 import { useActions } from "../../hooks/use-actions"
 import { useTypedSelector } from "../../hooks/use-typed-selector"
+import { useCumulativeCode } from "../../hooks/use-cumulative-code"
 
 interface codeCellProps {
   cell: Cell
@@ -16,20 +17,21 @@ interface codeCellProps {
 const CodeCell: React.FC<codeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions()
   const bundle = useTypedSelector((state) => state.bundle[cell.id])
-  const isBundle = !!bundle
+  const cumulativeCode = useCumulativeCode(cell.id)
 
   useEffect(() => {
-    if (!isBundle) {
-      createBundle(cell.id, cell.content)
+    if (!bundle) {
+      createBundle(cell.id, cumulativeCode)
       return
     }
     const timer = setTimeout(async () => {
-      createBundle(cell.id, cell.content)
+      createBundle(cell.id, cumulativeCode)
     }, 1250)
     return () => {
       clearTimeout(timer)
     }
-  }, [cell.content, cell.id, createBundle, isBundle])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cumulativeCode, cell.id, createBundle])
 
   return (
     <Resizable direction="vertical">
